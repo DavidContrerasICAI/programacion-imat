72fdef5d94c9b7cd275359c82616a77846994f5a
David Contreras <davidcb@comillas.edu>
2023-11-14 23:01:13 +0100

    Ejemplos avanzados de funciones

--- ejemplos_clase/08.funciones/ejemplos_avanzados/01.demo_funciones_argumentos/funciones.py
+++ ejemplos_clase/08.funciones/ejemplos_avanzados/01.demo_funciones_argumentos/funciones.py
@@ -1,9 +1,9 @@
-def init(mi_lista:list) -> None:
+def init(mi_lista:list) -> list:
     mi_lista.append("aaaa")
     mi_lista.append("bbbb")
     return mi_lista
 
-def add(my_list:list, cadena:str) -> None:
+def add(my_list:list, cadena:str) -> list:
     my_list.append(cadena)
     return my_list
 
