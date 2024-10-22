420fcf857476e9d371175b4f6b17c0514f226197
David Contreras <davidcb@comillas.edu>
2024-10-22 18:02:33 +0200

    Ejemplo de Listas

--- ejemplos_clase/07.listas/canciones.py
+++ ejemplos_clase/07.listas/canciones.py
@@ -3,11 +3,23 @@ print("CANTANTES")
 for cantante in cantantes:
     print(cantante)
 
-canciones_adele = ["Someone Like You", "Hello", "Rolling in the Deep", "Set Fire to the Rain", 
-                        "Easy On Me"]
-canciones_ed = ["Shape of You", "Perfect", "Thinking Out Loud", "Castle on the Hill", "Bad Habits"]
+canciones_adele = ["Someone Like You", "Hello", "Rolling in the Deep", "Set Fire to the Rain", "Easy On Me"]
 
+# Con la segunda lista de canciones se simula la entrada por teclado, por ejemplo, y se agrega una a una
+canciones_ed = []
+canciones_ed.append("Shape of You")
+canciones_ed.append("Perfect")
+canciones_ed.append("Thinking Out Loud")
+canciones_ed.append("Castle on the Hill") 
+canciones_ed.append("Bad Habits")
+
+# Opción 1 
 todas_canciones = [canciones_adele, canciones_ed]
+
+# Opción 2
+todas_canciones = []
+todas_canciones.append(canciones_adele)
+todas_canciones.append(canciones_ed)
 
 for i in range(len(todas_canciones)):
     print(cantantes[i])
